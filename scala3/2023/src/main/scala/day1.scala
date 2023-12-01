efa49a7603dc3629c42f3204c0f54b498175d7ac
Alexandru Nedelcu <noreply@alexn.org>
2023-12-01 08:47:25 +0200

    Use Vector.unfold

--- scala3/2023/src/main/scala/day1.scala
+++ scala3/2023/src/main/scala/day1.scala
@@ -1,47 +1,50 @@
-def part1: Unit =
+/*
+ * <https://adventofcode.com/2023/day/1>
+ */
+
+private def part1 =
     val regex = """\d""".r
-    val sum = io.Source
+    io.Source
         .fromResource("day1.txt")
-        .getLines()
+        .getLines
         .flatMap: line =>
-            val all = regex.findAllIn(line).toVector.map(_.toInt)
+            val all = regex.findAllIn(line).map(_.toInt).toVector
             for
                 f <- all.headOption
                 l <- all.lastOption
             yield f * 10 + l
         .sum
-    println(s"Sum of calibration values: $sum")
 
-@main def part2: Unit =
+private def part2 =
     val regex = """\d|one|two|three|four|five|six|seven|eight|nine""".r
     val digitsMap = Map(
-      "one" -> 1,
-      "two" -> 2,
+      "one"   -> 1,
+      "two"   -> 2,
       "three" -> 3,
-      "four" -> 4,
-      "five" -> 5,
-      "six" -> 6,
+      "four"  -> 4,
+      "five"  -> 5,
+      "six"   -> 6,
       "seven" -> 7,
       "eight" -> 8,
-      "nine" -> 9
+      "nine"  -> 9
     )
-
-    def findDigits(line: String, acc: Vector[String] = Vector.empty): Vector[Int] =
-        regex.findFirstMatchIn(line) match
-        case None =>
-            acc.map(v => digitsMap.getOrElse(v, v.toInt))
-        case Some(m) =>
-            findDigits(line.substring(m.start + 1), acc :+ m.matched)
-
-    val sum = io.Source
+    io.Source
         .fromResource("day1.txt")
-        .getLines()
+        .getLines
         .flatMap: line =>
-            val all = findDigits(line)
+            val all = Vector.unfold(line): line =>
+                for
+                    m <- regex.findFirstMatchIn(line)
+                    ds = m.matched
+                    di = digitsMap.getOrElse(ds, ds.toInt)
+                yield (di, line.substring(m.start + 1))
             for
                 f <- all.headOption
                 l <- all.lastOption
             yield f * 10 + l
         .sum
 
-    println(s"Sum of calibration values: $sum")
+@main def day1: Unit =
+    println("\nDay 1\n------------")
+    println(s"Part 1: $part1")
+    println(s"Part 2: $part2\n")
