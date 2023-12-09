9692c170eb95d56d1d020971ca446caa51c09432
Alexandru Nedelcu <noreply@alexn.org>
2023-12-09 08:18:37 +0200

    2023 / scala / day9

---
+++ scala3/2023/src/main/scala/day9.scala
@@ -0,0 +1,31 @@
+package aoc2023.day9
+
+val input = io.Source.fromResource("day9.txt")
+    .getLines
+    .to(LazyList)
+    .map(_.split("\\s+").map(_.toLong).toList)
+
+def extrapolate(history: List[Long]): (Long, Long) =
+    val all =
+        var step = history
+        var all  = List.empty[List[Long]]
+        while step.exists(_ != 0) do
+            all = step :: all
+            step = step.zip(step.tail).map((x, y) => y - x)
+        all
+
+    all.foldLeft((0L, 0L)):
+        case ((exFirst, exLast), line) =>
+            val first = line.head - exFirst
+            val last  = line.last + exLast
+            (first, last)
+
+@main def run =
+    val (part2, part1) = input.map(extrapolate).foldLeft((0L, 0L)):
+        case ((fs, ls), (first, last)) =>
+            (fs + first, ls + last)
+
+    println("\nDay 9")
+    println("---------------")
+    println(s"Part 1: $part1")
+    println(s"Part 2: $part2\n")
